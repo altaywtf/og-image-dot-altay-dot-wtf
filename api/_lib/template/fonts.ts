2d1555d42c3b0a196f66badb3d20139d54351b82
Altay Aydemir <altay@aydemir.io>
2021-08-18 04:40:39 +0200

    update font paths

--- api/_lib/template/fonts.ts
+++ api/_lib/template/fonts.ts
@@ -6,18 +6,8 @@ export const loadFont = (weightString: string, weightNumber: number) => `
   @font-face {
     font-family: '${FONT_FAMILY}';
     font-display: swap;
-    src: url('${FONTS_ROOT}/${FONT_FAMILY}/${FONT_FAMILY}-${weightString}.woff2') format('woff2'),
-        url('${FONTS_ROOT}/${FONT_FAMILY}/${FONT_FAMILY}-${weightString}.woff') format('woff');
+    src: url('${FONTS_ROOT}/${FONT_FAMILY}-${weightString}.woff2') format('woff2');
     font-weight: ${weightNumber};
     font-style: normal;
-  }
-
-  @font-face {
-    font-family: '${FONT_FAMILY}';
-    font-display: swap;
-    src: url('${FONTS_ROOT}/${FONT_FAMILY}/${FONT_FAMILY}-${weightString}-Italic.woff2') format('woff2'),
-        url('${FONTS_ROOT}/${FONT_FAMILY}/${FONT_FAMILY}-${weightString}-Italic.woff') format('woff');
-    font-weight: ${weightNumber};
-    font-style: italic;
   }
 `
